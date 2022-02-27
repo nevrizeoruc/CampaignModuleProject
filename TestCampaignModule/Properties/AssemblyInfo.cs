f121ab53364d323f9ff555e3bc81d1a40c531a66
nevrizeoruc <34655774+nevrizeoruc@users.noreply.github.com>
2022-02-27 16:31:15 +0300

    Campaign module project completed
    Campaign module project completed

---
+++ TestCampaignModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCampaignModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCampaignModule")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aede95f1-fcf0-46d1-babb-f4c0ad86e33f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCampaignModule/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using CampaignModule;
+using CampaignModule.Commands;
+using CampaignModule.Factories;
+using CampaignModule.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestCampaignModule
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static ProductRepository productRepo = ProductRepositoryFactory.GetInstance();
+        static CampaignRepository campaignRepo = CampaignRepositoryFactory.GetInstance();
+        static OrderRepository orderRepo = OrderRepositoryFactory.GetInstance();
+        static DateTime dateTimeProperty = new DateTime(0);
+
+        [TestMethod]
+        public void CreateProduct()
+        {
+            ProductCreateCommand command = new ProductCreateCommand("P1", 100, 1000);
+
+            Product product = new Product(command.ProductCode, command.Price, command.Stock);
+          
+            productRepo.Create(product);
+        }
+
+        [TestMethod]
+        public void GetProductInfo()
+        {
+            productRepo.GetInfo("P1");
+        }
+
+        [TestMethod]
+        public void CreateOrder()
+        {
+            OrderCreateCommand command = new OrderCreateCommand("P1",3);
+
+            Order order = new Order(command.ProductCode, command.Quantity);
+
+            orderRepo.Create(order);
+        }
+
+        [TestMethod]
+        public void GetOrder()
+        {
+            orderRepo.GetInfo("P1");
+        }
+
+        [TestMethod]
+        public void CreateCampaign()
+        {
+            CampaignCreateCommand command = new CampaignCreateCommand("C1", "P1", 10, 20, 100);
+
+            Campaign campaign = new Campaign(command.Name, command.ProductCode, command.Duration, command.PriceManipulationLimit, command.TargetSalesCount);
+
+            campaignRepo.Create(campaign);
+        }
+
+        [TestMethod]
+        public void GetCampaignInfo()
+        {
+            campaignRepo.GetInfo("C1");
+        }
+
+        [TestMethod]
+        public void IncreaseTime()
+        {
+            dateTimeProperty = dateTimeProperty.AddHours(1);
+
+            string endHour = dateTimeProperty.ToString("hh:mm");
+            Console.WriteLine($"Time is {endHour}");
+        }
+    }
+}
